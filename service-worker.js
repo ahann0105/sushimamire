52a8e54ce31d7f4271c3a506f64ee32b90518d90
ahann0105 <watanabe0105@icloud.com>
2026-05-25 22:22:46 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "sushimamire-v6";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sushi-images/tamago.png",
+  "./sushi-images/inari.png",
+  "./sushi-images/kappa.png",
+  "./sushi-images/tekkamaki.png",
+  "./sushi-images/ebi.png",
+  "./sushi-images/ikura.png",
+  "./sushi-images/uni.png",
+  "./sushi-images/maguro.png",
+  "./sushi-images/otoro.png",
+  "./sushi-images/oke.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
